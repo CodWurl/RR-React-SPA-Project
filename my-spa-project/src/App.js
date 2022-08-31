853f48ad83dbea8778c60961b186a4aea34ba70a
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-08-31 19:56:07 -0400

    added navbar

---
+++ my-spa-project/src/App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import './App.css';
+import { useSelector, useDispatch} from 'react-redux'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  const dispatch = useDispatch();
+  const mode = useSelector (state => state);
+
+  return (
+    <div className="App">
+     <NavBar></NavBar>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-spa-project/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useSelector} from 'react-redux'
+
+function NavBar() {
+    const mode = useSelector (state => state);
+
+
+    return (
+        <div className="NavBar">
+            <NavBar content="Blog" />
+            <NavBar content="About Me" />
+            <NavBar content="Projects" />
+            <NavBar content="contact" />
+        </div>
+    )
+};
+
+export default NavBar
