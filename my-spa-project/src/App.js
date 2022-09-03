8ca40186fb36b51e9b0977bf56bb66d7fc22a9f7
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-09-03 06:29:08 -0400

    made changes to app css

--- my-spa-project/src/App.js
+++ my-spa-project/src/App.js
@@ -4,18 +4,22 @@ import { useSelector, useDispatch } from 'react-redux'
 
 
 function App() {
- 
+  const dispatch = useDispatch();
+  const mode = useSelector (state => state);
+  
+  
+  const onModeChange = () => {
+    const actionCreator = mode.darkMode ? setLightMode : setDarkMode;
+    dispatch(actionCreator())
+  }
   
   return (
 
     
-
     <div>
       <Nav />
       
     </div>
-
-    
     
   );
 }
