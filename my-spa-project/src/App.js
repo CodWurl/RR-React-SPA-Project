db79da3bbc15b5d5c09e3f474f0d4ab0bea72b04
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-09-03 06:53:53 -0400

    changes

--- my-spa-project/src/App.js
+++ my-spa-project/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Nav from './components/Nav'
+//import ContentWrapper from './components/ContentWrapper'
+//import Footer from './components/Footer'
 import { useSelector, useDispatch } from 'react-redux'
-
+//import {setDarkMode, setLightMode} from './features/modeSlice';
 
 function App() {
   const dispatch = useDispatch();
   const mode = useSelector (state => state);
+  
   
   
   const onModeChange = () => {
@@ -14,14 +17,28 @@ function App() {
   }
   
   return (
-
-    
     <div>
       <Nav />
       
     </div>
-    
   );
 }
+
+const myStyle={
+  backgroundImage: 
+"url('https://media.geeksforgeeks.org/wp-content/uploads/rk.png')",
+  height:'100vh',
+  marginTop:'-70px',
+  fontSize:'50px',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+};
+return (
+<div style={myStyle}>
+  <h1> geeksforgeeks </h1>
+</div>
+);
+
+
 
 export default App;
